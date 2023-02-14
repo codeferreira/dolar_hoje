b3926ac24879524ab0e2895a79251f25c9a90f71
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2023-02-14 00:45:58 -0300

    create command to get dolar ratio to real

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use reqwest;
+use scraper::{Html, Selector};
+
 fn main() {
-    println!("Hello, world!");
+    let response = reqwest::blocking::get("https://dolarhoje.com")
+        .unwrap()
+        .text()
+        .unwrap();
+
+    let document = Html::parse_document(&response);
+
+    let dolar_value_selector = Selector::parse("#estrangeiro").unwrap();
+    let real_value_selector = Selector::parse("#nacional").unwrap();
+
+    let dolar = document
+        .select(&dolar_value_selector)
+        .next()
+        .unwrap()
+        .value()
+        .attr("value")
+        .unwrap();
+
+    let real = document
+        .select(&real_value_selector)
+        .next()
+        .unwrap()
+        .value()
+        .attr("value")
+        .unwrap();
+
+    println!("$ {} = R$ {}", dolar, real);
 }
